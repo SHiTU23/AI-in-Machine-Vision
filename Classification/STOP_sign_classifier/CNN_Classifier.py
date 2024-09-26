942abeb88730124a3deb02686bbdd9a76defba56
SHiTU23 <shi.tu2323@gmail.com>
2024-09-26 09:37:13 +0330

    train data with multiple layers

--- Classification/STOP_sign_classifier/CNN_Classifier.py
+++ Classification/STOP_sign_classifier/CNN_Classifier.py
@@ -3,6 +3,8 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
 currect_script_path = os.path.dirname(__file__)
 images_dir_path = os.path.join(currect_script_path, 'images')
@@ -72,3 +74,66 @@ train = data.take(train_size) ### allocate 4 batches of data to train
 val = data.skip(train_size).take(val_size) ### skip the batches that was alocated to train
 test = data.skip(train_size + val_size).take(test_size)
 print(len(train))
+
+##################################################
+###                 TRAIN DATA                 ###
+##################################################
+model = Sequential()
+
+#####################################
+###           ADD LAYERS          ###
+#####################################
+### First layer:
+"""
+ adding convolutional layer and max pooling layer
+ the first layer needs an input
+ the first convolution has 16 filters of 3 pixels by 3pixels in size
+ and these filters are going to move 1 pixel each time
+ input_shape is the size of images with 3 color channel 
+"""
+model.add(Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D()) ## take out the max value of the 2 by 2 region
+
+### Second layer:
+model.add(Conv2D(32, (3, 3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+### Third layer:
+model.add(Conv2D(16, (3, 3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+### we want to reach to a single value. So dont want the convolutional channel values
+model.add(Flatten())
+
+### Fully connected layers
+model.add(Dense(256, activation='relu'))
+### Final layer: map the output into 0 and 1
+model.add(Dense(1, activation='sigmoid'))
+
+### adam is a tf.optimazer
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+print(model.summary())
+
+#####################################
+###           TRAIN DATA          ###
+#####################################
+logdir = os.path.join(currect_script_path, 'logs')
+## save check points and logs
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=10, validation_data=val, callbacks=[tensorboard_callback])
+print(hist)
+
+#####################################
+###      PLOT THE PERFORMANCE     ###
+#####################################
+loss_fig = plt.figure()
+plt.plot(hist.history['loss'], color='teal', label='loss')
+plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
+loss_fig.suptitle("Loss", fontsize=20)
+plt.legend(loc="upper left")
+fig = plt.figure()
+plt.plot(hist.history['accuracy'], color='teal', label='accuracy')
+plt.plot(hist.history['val_accuracy'], color='orange', label='val_accuracy')
+fig.suptitle("Accuracy", fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
